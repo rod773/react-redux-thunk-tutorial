5fe274ea169e8d95a404560e3b4e89510b184f5e
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-18 15:39:13 +0100

    applyMiddleware(thunk),

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 
 import reducers from "./reducers/index";
 
 const store = createStore(
   reducers,
-  {},
+  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
