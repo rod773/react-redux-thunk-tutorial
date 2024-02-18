d3a15047bd8179363a66d5d3f07c90221e818b06
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-18 15:36:54 +0100

    redux thunk

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -2,30 +2,30 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 import axios from "axios";
-import { setProducts } from "./../redux/actions/productActions";
+import { setProducts, fetchProducts } from "./../redux/actions/productActions";
 
 const ProductListing = () => {
   const dispatch = useDispatch();
 
-  const fetchProducts = async () => {
-    const url = "https://fakestoreapi.com/products/";
+  // const fetchProducts = async () => {
+  //   const url = "https://fakestoreapi.com/products/";
 
-    const config = {
-      method: "GET",
-      url: url,
-      headers: {
-        "Cpntent-Type": "aplication/json",
-        Acept: "aplication/json",
-      },
-    };
+  //   const config = {
+  //     method: "GET",
+  //     url: url,
+  //     headers: {
+  //       "Cpntent-Type": "aplication/json",
+  //       Acept: "aplication/json",
+  //     },
+  //   };
 
-    const response = await axios(config).catch((error) => console.log(error));
+  //   const response = await axios(config).catch((error) => console.log(error));
 
-    dispatch(setProducts(response.data));
-  };
+  //   dispatch(setProducts(response.data));
+  // };
 
   useEffect(() => {
-    fetchProducts();
+    dispatch(fetchProducts());
   }, []);
 
   return (
--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,4 +1,23 @@
 import { ActionTypes } from "./../constants/actionTypes";
+import { axios } from "axios";
+
+export const fetchProducts = async () => {
+  const config = {
+    method: "GET",
+    url: "/products",
+    headers: {
+      "Cpntent-Type": "aplication/json",
+      Acept: "aplication/json",
+    },
+  };
+
+  const response = await axios(config).catch((error) => console.log(error));
+
+  return {
+    type: ActionTypes.FETCH_PRODUCTS,
+    payload: response,
+  };
+};
 
 export const setProducts = (products) => {
   return {
